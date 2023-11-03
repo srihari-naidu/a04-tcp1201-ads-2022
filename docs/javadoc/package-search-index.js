b2f4ffb995b504a373e28eead48490e5444cc0da
srihari-naidu <sriharixnaidu@gmail>
2023-11-03 15:01:01 +0800

    Added docs

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"helpers"},{"l":"Home"},{"l":"Login"},{"l":"models"},{"l":"Register"},{"l":"Role"}];updateSearchResults();
